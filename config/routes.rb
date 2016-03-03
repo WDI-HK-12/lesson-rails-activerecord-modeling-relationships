205776f782c863f80899490dc5ef65b8eeca92c9
Willie Tong <tongsinyin@gmail.com>
2016-03-03 15:44:59 +0800

    inclass solutions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :songs
   resources :managers
   resources :artists
+  resources :albums
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
